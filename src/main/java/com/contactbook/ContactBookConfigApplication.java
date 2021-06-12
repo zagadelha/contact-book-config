e2b2ece2ee4d4fe9f8019803d55205c4f811c158
Marcio Gadelha <zagadelha@gmail.com>
2021-06-12 18:03:32 -0300

    Implementations

--- src/main/java/com/contactbook/ContactBookConfigApplication.java
+++ src/main/java/com/contactbook/ContactBookConfigApplication.java
@@ -2,9 +2,9 @@ package com.contactbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
-@EnableEurekaClient
+@EnableConfigServer
 @SpringBootApplication
 public class ContactBookConfigApplication {
 
